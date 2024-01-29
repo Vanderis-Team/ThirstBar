53b355e2258efb5f19fd1bb5ddf8c54888e1722f
Sitonai <huynhphap100@gmail.com>
2024-01-29 18:40:25 +0700

    add placeholder stage current

--- src/main/java/me/orineko/thirstbar/manager/api/PlaceholderAPI.java
+++ src/main/java/me/orineko/thirstbar/manager/api/PlaceholderAPI.java
@@ -3,9 +3,12 @@ package me.orineko.thirstbar.manager.api;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import me.orineko.thirstbar.ThirstBar;
 import me.orineko.thirstbar.manager.player.PlayerData;
+import me.orineko.thirstbar.manager.stage.Stage;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class PlaceholderAPI extends PlaceholderExpansion {
 
@@ -29,6 +32,9 @@ public class PlaceholderAPI extends PlaceholderExpansion {
         PlayerData playerData = ThirstBar.getInstance().getPlayerDataList().addData(player);
         String identifier = params.toLowerCase();
         switch (identifier){
+            case "stage":
+                List<Stage> stageList = playerData.getStageCurrentList();
+                return String.valueOf((stageList.size() > 0) ? stageList.get(stageList.size()-1) : "");
             case "current_int":
                 return String.valueOf((int) playerData.getThirst());
             case "current_float":
